518346db1eedd7239409de9a8bf9a39c529d7b11
unknown <fulopmarton99@gmail.com>
2024-07-03 21:53:26 +0300

    Cloudflare tunnel integration

--- index.py
+++ index.py
@@ -2,6 +2,11 @@ from dotenv import load_dotenv
 import os
 # Use the package we installed
 from slack_bolt import App
+from pycloudflared import try_cloudflare
+
+tc = try_cloudflare(port=3000)
+
+print(tc.tunnel)
 
 load_dotenv()  # take environment variables from .env.
 
@@ -12,3 +17,9 @@ app = App(
 
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
+
+@app.command("/echo")
+def repeat_text(ack, respond, command):
+    # Acknowledge command request
+    ack()
+    respond(f"{command['text']}")
